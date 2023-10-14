f1351a987c00b2dc18aaff4d7e0c5844717d76aa
Ashwini Gawali <147256879+gawaliashwini@users.noreply.github.com>
2023-10-14 10:40:25 +0530

    AreaOfSquare.java

--- AreaOfSquare.java
+++ AreaOfSquare.java
@@ -1,11 +1,19 @@
+/*
+Write a program to calculate area of Square
+*/
+
 package lab1;
+
+import java.util.Scanner;
 
 public class AreaOfSquare {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	int n,area;
-	n=4;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter radius:");
+	n=sc.nextInt();
 	area=n*n;
 	System.out.println("Area of Square:"+area);
 	
