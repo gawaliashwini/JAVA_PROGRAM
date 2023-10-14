05e4d535a82ab9ade4c85ddca067d0d1595dfb8e
Ashwini Gawali <147256879+gawaliashwini@users.noreply.github.com>
2023-10-14 10:28:37 +0530

    AreaOFCircle.java

---
+++ AreaOFCircle.java
@@ -0,0 +1,16 @@
+/*
+Write a program to calculate Area Of Circle
+*/
+package lab1;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float area,radius;
+		radius=12;
+		area=3.14f*radius*radius;
+		System.out.println("Area of Circle:"+area);
+	}
+
+}
